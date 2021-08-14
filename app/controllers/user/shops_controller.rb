6982f8aacca4d4f9edfdbbf0405cd19e1c653c7e
EC2 Default User <ec2-user@ip-172-31-55-182.ec2.internal>
2021-08-14 14:19:36 +0000

    create user layouts

--- app/controllers/user/shops_controller.rb
+++
@@ -1,2 +0,0 @@
-class User::ShopsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,31 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get '/cart_products' => 'cart_products#index'
+    get 'cart_products/update'
+    get 'cart_products/create'
+    get 'cart_products/destroy'
+    get 'cart_products/all_destroy'
+    get 'users/my_page' => 'users#show'
+    get 'users/edit'
+    get 'users/update'
+    get 'users/unsubscribe'
+    get 'users/unsubscribe_update'
+    get '/shops' => 'shops#index'
+    get 'shops/show'
+    get '/products' => 'products#index'
+    get 'products/show'
+  end
   root to: 'homes#top'
+  
+  
   
   devise_for :users, :controllers => {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
     registrations: 'users/registrations'
 } 
-
-
+ 
+ 
 
   devise_for :admins, :controllers => {
     sessions:      'admins/sessions',
