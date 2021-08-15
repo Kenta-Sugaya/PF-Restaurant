8e983fd2de69e771a5e0871833a61e134d21ca0b
EC2 Default User <ec2-user@ip-172-31-55-182.ec2.internal>
2021-08-15 15:46:07 +0000

    repair products

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,17 @@
 class Public::ProductsController < ApplicationController
+  before_action :authenticate_user!,except: [:index, :show]
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:product_id])
+    @cart_products = CartProduct.new
   end
+  
+  private
+  def product_params
+    params.require(:product).permit(:name, :introduction, :price, :image, :genre_id, :amount)
+  end
+
 end
