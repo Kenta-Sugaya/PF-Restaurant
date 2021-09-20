b5957ab19ab14a0b4e711864d7955f63b82f268e
EC2 Default User <ec2-user@ip-172-31-55-182.ec2.internal>
2021-09-20 05:50:18 +0000

    add product link

--- app/controllers/public/shops_controller.rb
+++ app/controllers/public/shops_controller.rb
@@ -2,13 +2,18 @@ class Public::ShopsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
     @shops = Shop.all.order("id ASC")
+    @products = Product.all.order("id ASC")
   end
 
   def show
     @shop = Shop.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
   def shop_params
     params.require(:shop).permit(:shop_name, :shop_introduction, :shop_image)
+  end
+  def product_params
+    params.require(:product).permit(:name, :introduction, :price, :image, :amount)
   end
 end
